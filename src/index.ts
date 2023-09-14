3b79b2ebe6ff7e65906eb362aed2fcf07b89dabf
jefti <jeftimeira@gmail.com>
2023-09-14 15:50:32 -0300

    index config

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,8 @@
-import pg from "pg";
-import * as dotenv from 'dotenv';
+import express, { Request, Response, json } from "express";
+import httpStatus from "http-status";
 
-dotenv.config();
-const {Pool} = pg;
+const app = express();
+app.use(json());
 
-const connection = new Pool({
-    host: "localhost",
-    port: 5432,
-    user: "postgres",
-    password: process.env.DB_PASSWORD,
-    database:"rendimentos_poupança"
-});
-
-export {connection};
+const port: Number = Number(process.env.PORT) || 5000;
+app.listen(port, ()=>{ console.log(`Server running on port ${port}...`)});
